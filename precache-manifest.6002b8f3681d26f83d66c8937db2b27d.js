453fe90fe3555567fcd1869304df1a5f58bbcdd5
3rfanian <hooman.erfan@mail.polimi.it>
2020-07-29 11:17:29 +0430

    Updates

---
+++ precache-manifest.6002b8f3681d26f83d66c8937db2b27d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2966861da1b870594fed3338f8d8ae",
+    "url": "/RSE-recipe-search-engine/index.html"
+  },
+  {
+    "revision": "c55e8f18af9201eba2a4",
+    "url": "/RSE-recipe-search-engine/static/css/main.ab652a12.chunk.css"
+  },
+  {
+    "revision": "92756991f722857ec6ca",
+    "url": "/RSE-recipe-search-engine/static/js/2.f3f70430.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RSE-recipe-search-engine/static/js/2.f3f70430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c55e8f18af9201eba2a4",
+    "url": "/RSE-recipe-search-engine/static/js/main.75936a43.chunk.js"
+  },
+  {
+    "revision": "93e9f8fb789c82025e2f",
+    "url": "/RSE-recipe-search-engine/static/js/runtime-main.ef5dbeae.js"
+  }
+]);
